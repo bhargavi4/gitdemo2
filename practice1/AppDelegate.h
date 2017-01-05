cc0afce6ed37e5e05f8fa20cff2fda4b75f76ff0
admin <kuppala@ADMINs-MacBook-Air.local>
2017-01-05 21:08:07 +0530

    Initial Commit

---
+++ practice1/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  practice1
+//
+//  Created by siva bhargavi on 05/01/17.
+//  Copyright © 2017 siva bhargavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ practice1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  practice1
+//
+//  Created by siva bhargavi on 05/01/17.
+//  Copyright © 2017 siva bhargavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
